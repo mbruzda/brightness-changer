aa7bb5dfecad3f9872c410ad483faef34ca0bf3a
mbruzda <marcin.bru@op.pl>
2021-02-03 19:42:30 +0100

    added slider

--- app.py
+++ app.py
@@ -2,36 +2,37 @@ import tkinter as tk
 import cogs
 
 class Application(tk.Frame):
-    def __init__(self, master=None, geometry='300x300'):
+    def __init__(self, master=None):
         super().__init__(master)
+
         self.master = master
-        self.pack()
+        self.master.geometry('300x100')
+        self.master.title('Change your brightness')
+        self.pack(fill='both', expand=True)
         self.create_widgets()
+        
         
     def create_widgets(self):
         self.switch = tk.Button(self, text='Switch Brightness', command = self.switch_kill)
-        self.switch.pack()
+        self.switch.place(relx=0.1, rely=0.7)
 
-        self.input = tk.Entry()
-        self.input.pack()
-        
-        self.luminance = tk.IntVar()
-        self.input["textvariable"] = self.luminance 
+        self.slider = tk.Scale(self, from_=0, to=100,orient="horizontal",length=250, variable='IntVar')
+        self.slider.pack(side='top', pady=10)
+        self.slider.set(cogs.get_brightness())
+        self.slider.bind("<ButtonRelease-1>", self.update_brightness)
 
-        self.input.bind('<Key-Return>',self.set_kill)
+        self.quit = tk.Button(self, text="Quit", command = self.master.destroy)
+        self.quit.place(relx=0.7, rely=0.7)
+
+    def update_brightness(self, luminance):
+        luminance=self.slider.get()
+        cogs.set(luminance)
      
     def switch_kill(self):
         cogs.switch()
         self.master.destroy()
 
-    def set_kill(self, luminance):
-        cogs.set(int(self.luminance.get()))
-        self.master.destroy()
-
-
 
 root = tk.Tk()
-root.geometry('300x300')
-root.title("Change your brightness")
 app = Application(master=root)
 app.mainloop()
--- cogs.py
+++ cogs.py
@@ -1,6 +1,7 @@
 from monitorcontrol import get_monitors
 
 
+
 def switch():
     for monitor in get_monitors():
         with monitor:
@@ -13,5 +14,14 @@ def set(brightness):
     for monitor in get_monitors():
         with monitor:
             monitor.set_luminance(brightness)
+
+def get_brightness():
+    with get_monitors()[0] as monitor:
+        return monitor.get_luminance()
+
+print(get_brightness())
+
+
+
 
 
